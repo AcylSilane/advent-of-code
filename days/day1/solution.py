1da72e3fc72a91951c0e38b41de5d8ba8dddfaa8
James Dean <24254612+AcylSilane@users.noreply.github.com>
2022-12-04 19:58:42 -0500

    Add day 1

---
+++ days/day1/solution.py
@@ -0,0 +1,13 @@
+# Advent of code 2022
+
+# File is small, let's just read it all into memory
+
+with open("input.csv", "r") as inp:
+    data = inp.read().split("\n\n")
+totals = [sum(map(int, str.split(entry))) for entry in data]
+
+# Highest
+print(sorted(totals)[-1])
+
+# Sum of first 3
+print(sum(sorted(totals)[-3:]))
