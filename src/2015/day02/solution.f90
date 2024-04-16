def171ab9ca998acc9049b01cf81da58ecba9fa1
James Dean <24254612+AcylSilane@users.noreply.github.com>
2024-04-16 00:06:35 -0400

    finish 2015 day 2

--- src/2015/day02/solution.f90
+++ src/2015/day02/solution.f90
@@ -3,7 +3,8 @@
 program day2
     implicit none
     integer, dimension(3) :: input_vector
-    integer :: total = 0
+    integer :: part1_total = 0
+    integer :: part2_total = 0
     integer, parameter :: input_unit = 10
     character(len=16) :: line
 
@@ -11,11 +12,13 @@ program day2
     do while (.true.)
         read(input_unit, '(a)', end=200) line
         input_vector = parse_input(line)
-        total = total + calc_wrapping_paper(input_vector(1), input_vector(2), input_vector(3))
+        part1_total = part1_total + calc_wrapping_paper(input_vector)
+        part2_total = part2_total + calc_ribbon_length(input_vector)
     end do
  
 200 close(input_unit)
-    print *, "Part 1: ", total
+    print *, "Part 1: ", part1_total
+    print *, "Part 2: ", part2_total
 
 contains
     pure function parse_input(line_in) result(vector)
@@ -37,28 +40,52 @@ contains
         read(line_in(num_start:line_length), "(I3)") vector(vec_index)
     end function
 
-    pure function calc_wrapping_paper(length, width, height) result(area)
+    pure function get_smallest_sides(vec) result(result)
         implicit none
-        integer, intent(in) :: length, width, height
-        integer :: area, small1, small2
+        integer, intent(in), dimension(3) :: vec
+        integer, dimension(2) :: result
 
-        small1 = length
-        small2 = width
-        if (small1 > small2) then
-            small1 = width
-            small2 = length
+        result(1) = vec(1)
+        result(2) = vec(2)
+        if (result(1) > result(2)) then
+            result(1) = vec(2)
+            result(2) = vec(1)
         endif
-        if (height < small1) then
-            small2 = small1
-            small1 = height
-        else if (height < small2) then
-            small2 = height
+        if (vec(3) < result(1)) then
+            result(2) = result(1)
+            result(1) = vec(3)
+        else if (vec(3) < result(2)) then
+            result(2) = vec(3)
         end if
-
-        area = (2 * length * width) &
-             + (2 * width * height) &
-             + (2 * height * length) &
-             + (small1 * small2)
     end function
+
+    pure function calc_wrapping_paper(vec) result(area)
+        implicit none
+        integer, intent(in), dimension(3) :: vec
+        integer, dimension(2) :: smallest_sides
+        integer :: area
+        smallest_sides = get_smallest_sides(vec)
+        area = (2 * vec(1) * vec(2)) &
+             + (2 * vec(2) * vec(3)) &
+             + (2 * vec(3) * vec(1)) &
+             + (smallest_sides(1) * smallest_sides(2))
+    end function
+
+    pure function calc_ribbon_length(vec) result(ribbon)
+        implicit none
+        integer, intent(in), dimension(3) :: vec
+        integer, dimension(2) :: smallest_sides
+        integer :: ribbon
+
+        smallest_sides = get_smallest_sides(vec)
+        ribbon = 2 * smallest_sides(1) + 2*smallest_sides(2) + calc_volume(vec)
+    end function calc_ribbon_length
+
+    pure function calc_volume(vec) result(volume)
+        implicit none
+        integer, intent(in), dimension(3) :: vec
+        integer :: volume
+        volume = vec(1) * vec(2) * vec(3)
+    end function calc_volume
 
 end program day2
